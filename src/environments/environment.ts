365ef522f291404381764748d4eb7e5548527cdf
netlifyjoe <joe.keyes@netlify.com>
2024-08-26 12:24:43 -0400

    Initial Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+   contentfulSpaceId: 'ai3ozxv6c2ol',
+   contentfulPreviewToken: 'UyVPgferjKBwwuI9ihX6X79OXWNmsBdPUkfiHBF9b8s',
+   contentfulDeliveryToken: 'XabARu2qYETmahxElVjX1ss-QLeCQBUs5N0K6nkC7jY',
+   production: 'false'
+};
